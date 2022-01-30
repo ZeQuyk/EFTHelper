7194f09764dcbfe836b47063b4b5188d17c4e317
C1rdec <cedric_lampron@hotmail.com>
2022-01-30 12:11:14 -0500

    Add icon example

--- src/ViewModels/ItemTypeViewModel.cs
+++ src/ViewModels/ItemTypeViewModel.cs
@@ -2,6 +2,7 @@
 using EFTHelper.Enums;
 using EFTHelper.Extensions;
 using EFTHelper.Models;
+using MahApps.Metro.IconPacks;
 
 namespace EFTHelper.ViewModels
 {
@@ -18,6 +19,7 @@ namespace EFTHelper.ViewModels
         public ItemTypeViewModel(ItemTypes itemType)
         {
             _itemType = itemType;
+            Icon = BuildIcon(_itemType);
         }
 
         #endregion
@@ -28,9 +30,44 @@ namespace EFTHelper.ViewModels
 
         public string Title => _itemType.ToString().ToSentence();
 
-        public string Icon { get; }
+        public object Icon { get; }
 
         public ItemTypes ItemType => _itemType;
+
+        #endregion
+
+        #region Methods
+
+        private static PackIconBase BuildIcon(ItemTypes itemType)
+        {
+            PackIconBase icon = null;
+            switch (itemType)
+            {
+                case ItemTypes.Any:
+                    icon = new PackIconFontAwesome() { Kind = PackIconFontAwesomeKind.AsteriskSolid };
+                    break;
+                case ItemTypes.Ammo:
+                    icon = new PackIconMaterial() { Kind = PackIconMaterialKind.Ammunition };
+                    break;
+                case ItemTypes.AmmoBox:
+                    icon = new PackIconRPGAwesome() { Kind = PackIconRPGAwesomeKind.AmmoBag };
+                    break;
+                case ItemTypes.Armor:
+                    icon = new PackIconRemixIcon() { Kind = PackIconRemixIconKind.TShirt2Fill};
+                    break;
+                case ItemTypes.Backpack:
+                    icon = new PackIconUnicons() { Kind = PackIconUniconsKind.Backpack };
+                    break;
+            }
+
+            if (icon != null) 
+            { 
+                icon.Width = 24;
+                icon.Height = 24;
+            }
+
+            return icon;
+        }
 
         #endregion
     }
